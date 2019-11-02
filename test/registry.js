4f0220d8f59a08802dc9bc198d05d61fe0244c83
Mike Seese <seesemichaelj@gmail.com>
2019-11-02 15:06:32 -0700

    get tests passing

--- test/registry.js
+++ test/registry.js
@@ -3,19 +3,32 @@ const GravatarRegistry = artifacts.require("GravatarRegistry");
 
 contract("GravatarRegistry", (accounts) => {
   let instance;
+  const ownerAccount = accounts[0];
+  const gravatarAccount = accounts[1];
 
-  before(async () => {
-    instance = await GravatarRegistry.deployed();
+  it("deploys a registry", async () => {
+    instance = await GravatarRegistry.deployed({ from: ownerAccount });
+    await instance.setMythicalGravatar({ from: ownerAccount });
+  });
+
+  it("fails to set a mythical gravatar from a non-owner", async () => {
+    try {
+      await instance.setMythicalGravatar({ from: gravatarAccount });
+    } catch (e) {
+      return;
+    }
+
+    assert.fail("expected to throw");
   });
 
   it("creates a gravatar", async () => {
     const nameExpected = "test";
     const urlExpected = "http://test.test";
     await instance.createGravatar(nameExpected, urlExpected, {
-      from: accounts[0]
+      from: gravatarAccount
     });
 
-    const gravatarResult = await instance.getGravatar(accounts[0]);
+    const gravatarResult = await instance.getGravatar(gravatarAccount);
     assert.equal(gravatarResult[0], nameExpected);
     assert.equal(gravatarResult[1], urlExpected);
   });
@@ -23,18 +36,22 @@ contract("GravatarRegistry", (accounts) => {
   it("updates a gravatar name", async () => {
     const nameExpected = "test2";
   
-    await instance.updateGravatarName(nameExpected);
+    await instance.updateGravatarName(nameExpected, {
+      from: gravatarAccount
+    });
 
-    const gravatarResult = await instance.getGravatar(accounts[0]);
+    const gravatarResult = await instance.getGravatar(gravatarAccount);
     assert.equal(gravatarResult[0], nameExpected);
   });
 
   it("updates a gravatar image", async () => {
     const urlExpected = "http://test2.test";
 
-    await instance.updateGravatarImage(urlExpected);
+    await instance.updateGravatarImage(urlExpected, {
+      from: gravatarAccount
+    });
 
-    const gravatarResult = await instance.getGravatar(accounts[0]);
+    const gravatarResult = await instance.getGravatar(gravatarAccount);
     assert.equal(gravatarResult[1], urlExpected);
   });
 });
